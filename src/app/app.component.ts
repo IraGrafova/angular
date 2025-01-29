bf7ce4900dca52140d29b941bbb34ccd4b55fc9b
i.grafova <i.grafova@ntc-hitech.ru>
2025-01-29 11:52:10 +0300

    add components header and card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { ProductComponent } from './product/product.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet],
+    imports: [HeaderComponent, ProductComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
 })
