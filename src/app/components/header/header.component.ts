08c67383f9f1f3f82e5fb24d9a6f83afc550dfda
i.grafova <i.grafova@ntc-hitech.ru>
2025-02-07 09:44:19 +0300

    add ViewContainerRef

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -18,4 +18,6 @@ export class HeaderComponent {
     'https://avatars.mds.yandex.net/i?id=19847df007428c4ffe90cc91e1d5c71431c145e9-11042380-images-thumbs&n=13';
 
   readonly menuClick = output<Event>();
+
+  readonly buyClick = output<Event>();
 }
--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,6 +1,6 @@
-import { ChangeDetectionStrategy, Component, model } from '@angular/core';
+import { ChangeDetectionStrategy, Component, viewChild, input, TemplateRef, ViewContainerRef } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDrawer, MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-sidenav',
@@ -14,11 +14,38 @@ export class SidenavComponent {
   // readonly isSidenavOpened = input(false);
   // readonly isSidenavopenedChange = output<boolean>();
 
-  readonly isSidenavOpened = model(false);
+  // readonly isSidenavOpened = model(false);
+
+  private readonly matDrawer = viewChild.required<MatDrawer>('drawer');
+
+  readonly navigationTemplate = input<TemplateRef<unknown>>();
+  readonly contentTemplate = input<TemplateRef<unknown>>();
+
+  private readonly navigationViewport = viewChild.required('navigationViewport', {
+    read: ViewContainerRef,
+  });
+
+  private readonly contentViewport = viewChild.required('contentViewport', {
+    read: ViewContainerRef,
+  });
+
+  constructor() {
+    setTimeout(() => {
+      const navigationTemplate = this.navigationTemplate();
+      if (navigationTemplate) {
+        this.navigationViewport().createEmbeddedView(navigationTemplate, {});
+      }
+
+      const contentTemplate = this.contentTemplate();
+      if (contentTemplate) {
+        this.contentViewport().createEmbeddedView(contentTemplate);
+      }
+    }, 1000);
+  }
 
   toggleSidenavOpened() {
     // this.isSidenavOpenedChange.emit(!this.isSidenavOpened());
-
-    this.isSidenavOpened.set(!this.isSidenavOpened());
+    // this.isSidenavOpened.set(!this.isSidenavOpened());
+    this.matDrawer().toggle();
   }
 }
